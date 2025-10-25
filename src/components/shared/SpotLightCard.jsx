5c64c990044d56082cf1e8c022f795b0f987778e
Piplip <dnguyenkhanh457@gmail.com>
2025-10-25 21:02:54 +0700

    enhance UI/UX

--- src/components/shared/SpotLightCard.jsx
+++ src/components/shared/SpotLightCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRef } from 'react';
-import './SpotlightCard.css';
+import "./SpotLightCard.css"
 
 const SpotlightCard = ({ children, className = '', spotlightColor = 'rgba(255, 255, 255, 0.25)' }) => {
     const divRef = useRef(null);
